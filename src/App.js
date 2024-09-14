34fd27b7768b789333d3691035f85b9fa3c5ccf7
Vishnu Gupta <manav.vivira@gmail.com>
2024-09-14 23:58:15 +0530

    to do final

--- src/App.js
+++ src/App.js
@@ -16,26 +16,48 @@ const App = () => {
     setInputList("");
   };
 
+  const deleteItem = (id) => {
+    console.log("deleted");
+  
+  setItems((olditems) => {
+    return olditems.filter((arrElm, index) => {
+      return index !==id;
+    });
+  });
+  };
+
   return (
     <>
       <div className="main_div">
         <div className="center_div">
           <br />
           <h1>Todo List</h1>
-          <input type="text" placeholder="Add a Items" value={inputlist} onChange={itemEvent} />
+          <input
+            type="text"
+            placeholder="Add a Items"
+            value={inputlist}
+            onChange={itemEvent}
+          />
           <button onClick={listOfItems}> + </button>
 
           <ol>
             {/* <li>{inputlist}</li> */}
 
-            {items.map((itemval) => {
-             return<TodoLists text={itemval}/>
+            {items.map((itemval, index) => {
+              return (
+                <TodoLists
+                  id={index}
+                  key={index}
+                  text={itemval}
+                  onSelect={deleteItem}
+                />
+              );
             })}
           </ol>
         </div>
       </div>
     </>
   );
-};
+  };
 
 export default App;
--- src/TodoLists.js
+++ src/TodoLists.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
+import { TiDelete } from "react-icons/ti";
 
 const TodoLists = (props) => {
   return (
     <>
-    <div className='todo_style'>
-   
-    
-    <li>  {props.text}</li>;
-    </div>
+      <div className="todo_style">
+      
+        <li> <TiDelete
+            className="cross"
+            onClick={() => {
+              props.onSelect(props.id);
+            }}
+          />{props.text}</li>;
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TodoLists
+export default TodoLists;
